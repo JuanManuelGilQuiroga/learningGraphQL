4228d21890945eab8d957160c2c35fab9bba65ba
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2025-01-30 16:00:30 -0500

    feat: :sparkles: Get one user by id

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -3,14 +3,25 @@ const {gql} = require('apollo-server')
 const typeDefs = gql`
 
     type User {
+        id: ID!
         name: String!
         username: String!
         age: Int!
-        nationality: String!
+        nationality: Nationality!
+        friends: [User]
     }
 
     type Query {
         users: [User!]!
+        user(id: ID!): User!
+    }
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
     }
 `;
 
